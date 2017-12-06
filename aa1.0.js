2be4b8ef848dcc9eb027b37aa4f27a097af846d5
zhangyi999 <467348631@qq.com>
2017-12-06 23:42:10 +0800

    Initial1

---
+++ aa1.0.js
@@ -0,0 +1,35 @@
+
+
+var a = [
+    [
+        ['id','主键'],
+        ['name','text','平台名称'],
+        ['detail','text','简介'],
+        ['minprice','text','最低额度'],
+        ['maxprice','text','最高额度'],
+        ['fasttime','text','最快放款时间']
+    ],
+    [   
+        ['interest','text','放款利率(月息%)'],
+        ['mintime','text','最低贷款期限（天）'],
+        ['maxtime','text','最长贷款期限（天）'],
+        ['condition','text','放款条件'],
+        ['material','text','所需材料'],
+        ['tag','text','标签'],
+        ['customer','text','客服电话'],
+        ['qq','text','客服QQ'],
+        ['img','text','图片地址'],
+        ['seenum','text','查看次数']
+    ],
+    [
+        ['charge','text','服务费'],
+        ['onecharge','text','一次性服务费'],
+        ['authentication','text','授权认证'],
+        ['examine','text','审核方式'],
+        ['repayment','text','还款方式']
+    ]
+]
+var h = ''
+
+a.map(function (v, i) {
+       });
